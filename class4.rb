734a3c40b2e785f08dae97f3e71484f3b4771e25
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 17:08:51 +0530

    add class4.rb in which i have declared constent var

---
+++ class4.rb
@@ -0,0 +1,21 @@
+# constent variable 
+
+class Contant_var
+	VAR1=10;
+	VAR2=20;
+
+	def show()
+		puts "VAR1 value :#{VAR1}";
+		puts "VAR2 value : #{VAR2}";
+	end
+
+end
+
+object1=Contant_var.new;
+
+object1.show;
+
+# in ruby optional to add small brekets in no arguments method
+# also optional add ;  end of line
+
+
