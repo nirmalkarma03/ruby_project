0fe845b5c80d320ad0d5413377815f635a7c97e0
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 15:07:36 +0530

    add END Keyword and commets

--- test2.rb
+++ test2.rb
@@ -1,6 +1,16 @@
+
+#this is comment 
+=begin 
+this project is usefull for learn ruby
+used to comment multipal line .
+=end
 puts "hii this is my first ruby program (karma)";
 
 BEGIN {
 
 	puts "it is like static block in java  (nirmal)";
+}
+
+END {
+	puts "this block is called end of program (....)";
 }
