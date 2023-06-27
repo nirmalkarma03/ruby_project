9dcd56113d88dc9e390a61c5a07e860eea52befd
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 13:23:26 +0530

    first commit

---
+++ test1.rb
@@ -0,0 +1,24 @@
+puts "hello fittonia";
+
+print <<EOF
+JAY RAM
+JAY SHREE RAM
+EOF
+
+print <<"EOF";
+
+RAM RAM SIYA RAM
+RAM RAM SIYA RAM
+EOF
+
+print <<`EOC` 
+echo ram
+echo shyam
+EOC
+
+print <<"foo" ,<<"bar"
+ i said foo
+foo
+ i said bar
+bar
+
