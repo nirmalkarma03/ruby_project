521159b32029e0c2053166480f3ad85ae0aafda7
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 22:35:24 +0530

    add use range (10..15), <=>, ===,eql?,equal?

--- class5.rb
+++ class5.rb
@@ -28,3 +28,64 @@ case $age
 	when (7..10)
 		puts " child"
 	end
+
+
+
+	a,b=1,2;
+	c=1;
+
+	puts a==b;
+
+	puts a==1;
+
+	puts 1==1.0;
+
+	puts 1.eql?(1.0);
+
+	puts a.equal?1
+
+	puts a.equal?b;
+
+	puts a.equal?c;
+
+	puts a.eql?c;
+
+	puts (1...3)===a;
+
+puts ""
+	puts a<=>c;
+
+	puts a<=>b;
+
+	puts a<=>0;
+
+puts "";
+
+	puts 1<2?"1 is small" : "1 is greter"
+
+	puts ""
+	(10..15).each do |i|
+		puts i;
+	end
+
+	(10...15).each do |i|
+	puts i;
+	end
+
+foo =23;
+
+puts defined?foo;
+
+puts defined?$age;
+
+puts defined?bar
+
+puts defined? puts
+
+
+
+	
+
+	
+
+
