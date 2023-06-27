2945ecbde039d12e4f7dae9c539d42981677dccf
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 22:04:00 +0530

    add class.rb in which i have used if,elsif,and case when

---
+++ class5.rb
@@ -0,0 +1,30 @@
+# use range 
+
+(1..10).each do |i|
+	puts i ;
+end
+
+
+x=1;
+
+if x>2
+	puts "x is grater";
+elsif x<2
+	puts " x is less"
+else
+	 puts "x equal"
+
+	end
+
+
+$age=5;
+
+case $age
+
+	when (1..4)
+		puts "baby"
+	when (4..6)
+		puts "little child"
+	when (7..10)
+		puts " child"
+	end
