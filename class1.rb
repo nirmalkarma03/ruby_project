f7e07f21467a6275e79b8cdab744c9754d14a2e6
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 15:35:31 +0530

    add class1.rb which i have learn how to create class and object

---
+++ class1.rb
@@ -0,0 +1,36 @@
+# this example of vehicle class
+
+
+=begin
+class vehicle{
+
+	Number wheel_count;
+	Characters name_v;
+	Characters color;
+
+	Function speed{
+
+	}
+
+	Function auto{
+
+	}
+}
+
+
+=end
+
+class Custmor
+
+	def initialize(id,name,addr)
+		@cus_id=id;
+		@cus_name=name;
+		@cus_addr=addr;
+		end
+	end
+
+	cust1=Custmor.new(1,"ajay","amaltha")
+	cust2=Custmor.new(2,"nirmal","amlatha")
+
+	puts cust1;
+	puts cust2;
