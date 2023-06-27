20ef84b90c8b3e206c044fe36319ddf6d8c551e6
nirmalkarma03 <karmanirmal3@gmail.com>
2023-06-27 15:41:35 +0530

    add class2.rb when i create object and call method

---
+++ class2.rb
@@ -0,0 +1,12 @@
+# i have crate Hello class and also define a method hello no argument and create object of this class 
+# and call method of this class  object.method_name
+
+class Hello
+	def hello
+		puts "hello Ruby !";
+	end
+end
+
+object=Hello.new;
+
+object.hello;
